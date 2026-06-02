88c12daa5dbb01cddffec4edd315fc6dbe2f10b3
nad811 <nad349393@gmail.com>
2026-06-02 14:22:03 +0530

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,13 @@
+def display_menu():
+
+    print("\nStudent Result System")
+
+    print("1. Add Student")
+
+    print("2. View Students")
+
+    print("3. Show Results")
+
+    print("4. Delete Student")
+
+    print("5. Exit")
